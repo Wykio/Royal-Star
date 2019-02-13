34eb3137bed02f2e6fe5a229736f15569a224568
Régis <regis.poizat@cegetel.net>
2019-02-13 19:43:46 +0100

    fin du prototype

--- Unity/Assets/MyPhotonProject/Scripts/test shipcontroller scripts/shipMotor.cs
+++ Unity/Assets/MyPhotonProject/Scripts/test shipcontroller scripts/shipMotor.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class shipMotor : MonoBehaviour
 {
     //potentiomètres pour bidouiller les stats du vaisseau
-    
+    [Header ("Potentiomètres du vaisseau")]
     [SerializeField] private float speed = 90f;
-    [SerializeField] private float turnSpeed = 5f;
     [SerializeField] private float hoverForce = 65f;
     [SerializeField] private float hoverHeight = 3.5f;
     [SerializeField] private float speedRotate = 100;
-    [SerializeField] private float damping = 0.95f;
+    [SerializeField] private float dampingHover = 0.85f;
+    [SerializeField] private float dampingSpeed = 0.95f;
     [SerializeField] private float dampingHeight = 4f;
 
     //composants du vaisseau
+    [Header ("Composants du vaisseau")]
     [SerializeField] private Transform[] hoverPoints;
     [SerializeField] private Transform centreGravite;
     private Transform shipTransform;
@@ -22,8 +23,6 @@ public class shipMotor : MonoBehaviour
 
     //variables pour les inputs
     private float powerInput;
-    private float lateralInput;
-    private float turnInput;
     private float sourisHorizontale;
     private float sourisVerticale;
     private bool Aerien = true;
@@ -44,57 +43,84 @@ public class shipMotor : MonoBehaviour
     void Update()
     {
         powerInput = Input.GetAxis("Vertical");
-        sourisHorizontale = -Input.GetAxis("Mouse Y");
-        sourisVerticale = Input.GetAxis("Mouse X");  
+        sourisVerticale = -Input.GetAxis("Mouse Y");
+        sourisHorizontale = Input.GetAxis("Mouse X");  
     }
 
     void FixedUpdate()
     {
         DetectionDuSol();
 
-        Debug.Log("Aerien = " + Aerien);
-
-        if (!Aerien)
-        {
-            Hover();
-            
-        }
+        //si le vaisseau est prêt du sol, la lévitation est enclenchée
+        if (!Aerien) Hover();
 
         //si le vaisseau est en l'air, les touches latérales le font rouler
         if(Aerien)
         {
-            if (Input.GetKey(KeyCode.Q))
+            //si le joueur maintient CRTL enfoncé, le boost antigrav est activé
+            if(Input.GetKey(KeyCode.LeftShift))
             {
-                shipTransform.Rotate(0, 0, -speedRotate * Time.deltaTime);
+                Debug.Log("antigrav activé");
+                shipRigidbody.AddForce(shipTransform.forward * (speed * 2f) * powerInput, ForceMode.Force);
+
+                //rotation horizontale et verticale avec la souris
+                if(sourisHorizontale != 0f)
+                {
+                    shipRigidbody.AddRelativeTorque(0, sourisHorizontale * (speed / 2f), 0);
+                }
+                if(sourisVerticale != 0f)
+                {
+                    shipRigidbody.AddRelativeTorque(-sourisVerticale * (speed / 2f), 0, 0);
+                }
             }
-            if (Input.GetKey(KeyCode.D))
+            else
             {
-                shipTransform.Rotate(0, 0, speedRotate * Time.deltaTime);
+                if (Input.GetKey(KeyCode.Q))
+                {
+                    shipTransform.Rotate(0, 0, speedRotate * Time.deltaTime);
+                }
+
+                if (Input.GetKey(KeyCode.D))
+                {
+                    shipTransform.Rotate(0, 0, -speedRotate * Time.deltaTime);
+                }
+                if (Input.GetKey(KeyCode.Z))
+                {
+                    shipTransform.Rotate(speedRotate * Time.deltaTime, 0, 0);
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    shipTransform.Rotate(-speedRotate * Time.deltaTime, 0, 0);
+                }
             }
-            if (Input.GetKey(KeyCode.Z))
-            {
-                shipTransform.Rotate(speedRotate * Time.deltaTime, 0, 0);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                shipTransform.Rotate(-speedRotate * Time.deltaTime, 0, 0);
-            }
+
         }
         else
         {
+            // propulsion avant Z
             shipRigidbody.AddForce(shipTransform.forward * powerInput * propulsionAvantAppliquee, ForceMode.Force);
 
-            //sinon elles le font strafer
-            if (Input.GetKey(KeyCode.Q))
-            {
-                shipRigidbody.AddForce(-(speed) * Time.deltaTime, 0, 0, ForceMode.Impulse);
-            }
+            //rotation horizontale avec la souris
+            shipRigidbody.AddRelativeTorque(0, sourisHorizontale * (speed / 2f), 0);
+
+            //si le vaisseau est au sol, les touches latérales le font strafer
             if (Input.GetKey(KeyCode.D))
             {
-                shipRigidbody.AddForce((speed) * Time.deltaTime, 0, 0, ForceMode.Impulse);
+                shipRigidbody.AddRelativeForce((speed) * Time.deltaTime, 0, 0, ForceMode.Impulse);  
             }
+            else
+            {
+                if (Input.GetKey(KeyCode.Q))
+                {
+                    shipRigidbody.AddRelativeForce(-(speed) * Time.deltaTime, 0, 0, ForceMode.Impulse);
+                }
+            }  
         }
-        
+
+        //effet damping sur les vitesses de déplacement
+        shipRigidbody.velocity = new Vector3(shipRigidbody.velocity.x * dampingSpeed,
+        shipRigidbody.velocity.y,
+        shipRigidbody.velocity.z * dampingSpeed);
     }
 
     //On vérifie si le vaisseau est en chute libre ou en lévitation
@@ -110,7 +136,6 @@ public class shipMotor : MonoBehaviour
 
             if(Physics.Raycast(scan, out hit, hoverHeight + 0.5f))
             {
-                Debug.Log("collision trouvée : " + hit.collider.name);
                 Aerien = false;
             }
         }
@@ -161,10 +186,12 @@ public class shipMotor : MonoBehaviour
             if(distance > hoverHeight)
             {
                 shipRigidbody.velocity = new Vector3(shipRigidbody.velocity.x,
-                shipRigidbody.velocity.y * damping,
+                shipRigidbody.velocity.y * dampingHover,
                 shipRigidbody.velocity.z);
             }
         }
+        
+
     }
 
     //methode pour récupérer la moyenne des normales détectées par les capteurs
