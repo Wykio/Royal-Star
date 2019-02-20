4e6623f7278691a2e26e4b8c62395812b3752ed2
Unknown <antoine.thenevin@gmail.com>
2019-02-20 18:59:36 +0100

    Test_Basic_Network

---
+++ Unity/Assets/Royal Star/Scripts/PlayerManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Com.MyCompany.MyGame
+{
+    public class PlayerManager : MonoBehaviourPun
+    {
+
+        #region Public Fields
+
+            [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+            public static GameObject LocalPlayerInstance;
+
+        #endregion
+        
+        #region Private Fields
+
+        private Rigidbody rb;
+
+        #endregion
+    
+        #region Private Serializable Fields
+
+        [SerializeField] private float speed = 20;
+    
+        #endregion
+    
+        #region MonoBehaviour Callbacks
+
+        void Awake()
+        {
+            // #Important
+            // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+            if (photonView.IsMine)
+            {
+                PlayerManager.LocalPlayerInstance = this.gameObject;
+            }
+            // #Critical
+            // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            CameraWork _cameraWork = this.gameObject.GetComponent<CameraWork>();
+
+
+            if (_cameraWork != null)
+            {
+                if (photonView.IsMine)
+                {
+                    _cameraWork.OnStartFollowing();
+                }
+            }
+            else
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
+            }
+        }
+    
+        void Update()
+        {
+            if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
+            {
+                return;
+            }
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        
+            rb.AddForce(movement * speed);
+        }
+
+
+        void FixedUpdate()
+        {
+
+        }
+
+        #endregion
+    }
+}
