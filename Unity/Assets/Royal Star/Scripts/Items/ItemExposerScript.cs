0791f1a95aec09c9bc8e089cf1c4232c213719bb
Régis <regis.poizat@cegetel.net>
2019-06-09 23:41:15 +0200

    items destructibles par laser

--- Unity/Assets/Royal Star/Scripts/Items/ItemExposerScript.cs
+++ Unity/Assets/Royal Star/Scripts/Items/ItemExposerScript.cs
@@ -8,27 +8,37 @@ public class ItemExposerScript : MonoBehaviour
 
     private bool ramasse = false;
 
+    //activer le gameobject de l'item
     public void ActivationItem()
     {
         itemTransform.gameObject.SetActive(true);
     }
 
+    //désactiver le gameobject de l'item
     public void DesactivationItem()
     {
         itemTransform.gameObject.SetActive(false);
     }
 
+    //définir la position de l'item
     public void SetPosition(Vector3 position)
     {
         itemTransform.position = position;
     }
 
+    //fonction de collision entre un objet et l'item
     private void OnTriggerEnter(Collider other)
     {
+        //si le collider est un joueur
+        if(other.gameObject.tag == "Player")
+        {
+
+        }
         ramasse = true;
         DesactivationItem();
     }
 
+    //getteur pour avoir l'état de l'item, en place ou ramassé
     public bool getRamasse()
     {
         return ramasse;
