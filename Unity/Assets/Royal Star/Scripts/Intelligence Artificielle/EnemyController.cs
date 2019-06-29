9256d42239c809bdd926400e676d671313ab135b
wykio <antoine.thenevin@gmail.com>
2019-06-29 17:30:47 +0200

    Probleme avec le "set destination"

---
+++ Unity/Assets/Royal Star/Scripts/Intelligence Artificielle/EnemyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    // Portée de vue de l'IA
+    [SerializeField] float lookRadius = 10f;
+
+    private Transform target;
+    private NavMeshAgent agent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = PlayerManager.instance.player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    //affiche les sphéres de portée de vue
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
